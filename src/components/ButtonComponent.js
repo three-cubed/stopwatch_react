88fbbece64479bd37ffe11c3d75868e08dc0fff4
three-cubed <72698356+three-cubed@users.noreply.github.com>
2022-02-23 20:32:41 +0000

    minor edit: button text: "Stop"

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -4,7 +4,7 @@ const ButtonComponent = (props) => {
         <div id ='buttonComponent'>
             <br />
             <button onClick={props.runTimer}>Start</button>
-            <button onClick={props.stopTimer}>Pause / Stop</button>
+            <button onClick={props.stopTimer}>Stop</button>
             <button onClick={props.recordLapTime}>Record lap</button>
             <button className='btn-clr' onClick={props.clearTimer}>Clear all</button>
             <button className='btn-clr' onClick={props.clearLaps}>Clear laps</button>
